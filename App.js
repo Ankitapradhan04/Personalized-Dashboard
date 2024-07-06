34e735da21808ee4f951fb39e398f6d8c5e0ca2a
Ankita Pradhan <67707036+Ankitapradhan04@users.noreply.github.com>
2024-07-06 23:59:05 +0530

    Application

---
+++ App.js
@@ -0,0 +1,23 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
